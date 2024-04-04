d4eceb0c0a3f15ec6a5c7f997e4b01a2c3f13c7d
pmy0802 <pmy0802@naver.com>
2024-04-04 15:04:10 +0900

    계산기 완성

--- app/src/main/java/kr/ac/kopo/simplecalculator0328/MainActivity.kt
+++ app/src/main/java/kr/ac/kopo/simplecalculator0328/MainActivity.kt
@@ -2,9 +2,11 @@ package kr.ac.kopo.simplecalculator0328
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View.OnClickListener
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     lateinit var edit1:EditText
@@ -13,8 +15,9 @@ class MainActivity : AppCompatActivity() {
     lateinit var btnMinus:Button
     lateinit var btnMultiply:Button
     lateinit var btnDivide:Button
+    lateinit var btnMod:Button
     lateinit var textResult:TextView
-    var result : Int? = null
+    var result : Double? = null
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,33 +31,50 @@ class MainActivity : AppCompatActivity() {
         btnMinus = findViewById<Button>(R.id.btnMinus)
         btnMultiply = findViewById<Button>(R.id.btnMultiply)
         btnDivide = findViewById<Button>(R.id.btnDivide)
+        btnMod = findViewById<Button>(R.id.btnMod)
         textResult = findViewById<TextView>(R.id.textResult)
 
-        btnPlus.setOnClickListener{
-            val num1= edit1.text.toString().toInt()
-            val num2= edit2.text.toString().toInt()
-            result = num1 + num2
-            textResult.text = "계산 결과:" + result
-        }
-        btnMinus.setOnClickListener{
-            val num1= edit1.text.toString().toInt()
-            val num2= edit2.text.toString().toInt()
-            result = num1 - num2
-            textResult.text = "계산 결과:" + result
-        }
-        btnMultiply.setOnClickListener{
-            val num1= edit1.text.toString().toInt()
-            val num2= edit2.text.toString().toInt()
-            result = num1 * num2
-            textResult.text = "계산 결과:" + result
-        }
-        btnDivide.setOnClickListener{
-            val num1= edit1.text.toString().toFloat()
-            val num2= edit2.text.toString().toFloat()
-            var result : Float? = null
-            result = num1 / num2
-            textResult.text = "계산 결과:" + result
+        btnPlus.setOnClickListener(btnListener)
+        btnMinus.setOnClickListener(btnListener)
+        btnMultiply.setOnClickListener(btnListener)
+        btnDivide.setOnClickListener(btnListener)
+        btnMod.setOnClickListener(btnListener)
+
+    }
+
+    var btnListener = OnClickListener{
+        val num1Str = edit1.text.toString()
+        val num2Str = edit2.text.toString()
+
+        if(num1Str=="" || num2Str==""){
+            Toast.makeText(baseContext, "입력을 하지 않았습니다.", Toast.LENGTH_LONG).show()
+            return@OnClickListener
         }
 
+        val num1= num1Str.toDouble()
+        val num2= num2Str.toDouble()
+
+
+        when(it.id){
+            R.id.btnPlus->result = num1 + num2
+            R.id.btnMinus->result = num1 - num2
+            R.id.btnMultiply->result = num1 * num2
+            R.id.btnDivide-> {
+                if(num2 == 0.0){
+                    Toast.makeText(baseContext, "0으로 나누면 안됩니다.", Toast.LENGTH_LONG).show()
+                    return@OnClickListener
+                }
+                result = num1 / num2
+            }
+            R.id.btnMod->{
+                if(num2 == 0.0){
+                    Toast.makeText(baseContext, "0으로 나누면 안됩니다.", Toast.LENGTH_LONG).show()
+                    return@OnClickListener
+                }
+                result = num1 % num2
+            }
+        }
+
+        textResult.text = "계산 결과: " + result
     }
 }
